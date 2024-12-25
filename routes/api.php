3b31b207cd92a92cb26edd13c78b9633a548b99f
khauvannam <nkhauvan@gmail.com>
2024-12-25 14:11:36 +0700

    Add type hinting to tags relationship in Folder model
    Added `BelongsToMany` type hint to the `tags` method for improved clarity and static analysis. Adjusted import order for better code organization and readability.

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,16 @@
 <?php
 
+use App\Http\Controllers\Auth\UserController;
+use App\Http\Controllers\Histories\UserHistoryController;
+use App\Http\Controllers\TagController\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get("history",[UserHistoryController::class,"show"]);
+Route::get("history", [UserHistoryController::class, "show"]);
 
-
-    // Route API cho các hành động của TagController
-    Route::apiResource('tags', TagController::class);
-
+Route::apiResource('tags', TagController::class);
 
 Route::apiResource('user', UserController::class);
